ba284ade49b0df93bbeea87835cd31e3dfa7afdc
Maxime Poli <maxime.poli@eleves.enpc.fr>
2020-10-01 14:46:40 +0200

    revised

--- pykaldi-cpu/build.sh
+++ pykaldi-cpu/build.sh
@@ -1,5 +1,4 @@
 set -x
-
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
@@ -9,19 +8,6 @@ export CLIF_MATCHER="/root/pykaldi/tools/clif_backend/build_matcher/bin/clif-mat
 # Create lib folder
 LIB_FOLDER="$SP_DIR/kaldi/lib"
 mkdir -p $LIB_FOLDER
-
-# #####################################
-# # Copy cuda
-# #####################################
-DEPS_LIST=(
-	"/usr/local/cuda/lib64/libcublas.so*"
-	"/usr/local/cuda/lib64/libcudart.so*"
-	"/usr/local/cuda/lib64/libcurand.so*"
-	"/usr/local/cuda/lib64/libcusparse.so*"
-)
-for filepath in "${DEPS_LIST[@]}"; do
-	cp -av $filepath $LIB_FOLDER
-done
 	
 #####################################
 # Install kaldi locally
@@ -44,7 +30,6 @@ find $SP_DIR/kaldi/lib -maxdepth 1 -name "*.so*" -type f | while read sofile; do
 	echo "Setting rpath of $sofile to \$ORIGIN, conda/lib"
 	patchelf --set-rpath '$ORIGIN:$ORIGIN/../../../..' $sofile
 done
-
 
 #########################################
 # Force CMake to find correct python
