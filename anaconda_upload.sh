ba8a39a0377b246dd6bd7a352c0bdd92cd55ae11
Maxime Poli <44207491+mxmpl@users.noreply.github.com>
2020-10-02 10:20:45 +0200

    Update anaconda_upload.sh

--- anaconda_upload.sh
+++ anaconda_upload.sh
@@ -2,4 +2,4 @@
 BLD_PATH=/usr/local/conda-bld/linux-64/
 
 git pull origin master
-conda build -c coml --user coml --token $CONDA_TOKEN --skip-existing $1
+conda build -c coml --user coml --token $CONDA_UPLOAD_TOKEN --skip-existing $1
